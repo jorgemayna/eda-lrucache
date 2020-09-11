d7b136f23227c4247f5a425f86d67f32ce10ed77
Jorge Mayna Flores <jorgemayna@gmail.com>
2020-09-11 15:03:00 -0500

    templates, optional,node

--- lrucache.cpp
+++ lrucache.cpp
@@ -1,46 +1,119 @@
+/*
 
+//se uso unordered_map y list 
 
-//se uso unordered_map y vector como un queue
+*/
 #include <iostream>
 #include <unordered_map>
-#include <vector>
+#include <list>
 #include <string>
+#include <optional> 
+#include <functional>
 using namespace std;
 
-class LRUcache{
-  int maxSize;
-  unordered_map<string,int> cache;
-  vector<pair<string,int>> lru;
-
-  LRUcache(int size){
-    maxSize=size;
-  }
-  void insertKeyValuePair(string key , int value){
-    if(cache.count(key)==0){//la llave no existe
-      if(cache.size()<maxSize){//el cache no esta lleno
-        cache.insert(make_pair(key,value));
-        lru.push_back(make_pair(key,value));
-        return;
-      }else{              //el cache si esta lleno
-        cache.erase(lru.back().first);
-        lru.pop_back();
-        return;
-      }      
+template<class TT,class YY>
+class Node{  
+  public:
+    TT key;
+    YY value;
+    Node(TT key_ , YY value_){
+      key=key_;
+      value=value_;
     }
-    //la llave si existe
-    cache[key]=value;
-    //falta hacer que ese elemento pase al mas recientemente usado
-  }
-  int getValueFromKey(string key){
-    //falta hacer que ese elemento pase al mas recientemente usado
-    return cache[key];
-  }
-  string getMostRecentKey(){
-    return lru.front().first;
-  }
+};
+
+template<class T,class Y>
+class LRUcache{
+  private:
+    int maxSize;
+    unordered_map<T,typename list<Node<T,Y> >::iterator > cache;
+    list<Node<T,Y>> lru;
+  public:
+    LRUcache(int size){
+      maxSize=size;
+    }
+
+    void insertKeyValuePair(T key , Y value);      
+    optional<Y> getValueFromKey(T key);
+    T getMostRecentKey(){
+      return lru.front().key;      
+    }
+
+    void print_cache(){
+      cout<<"## Cache  ===="<<endl;
+      for(auto it = cache.begin();it != cache.end();it++){
+        cout << it->first<<" => ("<<it->second->key<<", "<<it->second->value<<")"<<endl;
+      }
+    }
+    void print_lru(){
+      cout<<"## LRU  ===="<<endl;
+      for(auto it = lru.begin();it!=lru.end();it++){
+        cout <<" ("<<it->key<<", "<<it->value<<")"<<endl;
+      }
+    }
 
 };
 
-int main(){
+template<class T,class Y>
+void LRUcache<T,Y>::insertKeyValuePair(T key , Y value){
+  if(cache.count(key)==0){//la llave no existe
+    if(cache.size()==maxSize){//el cache esta lleno                                  
+      cache.erase(lru.back().key);
+      lru.pop_back();          
+    }      
+    lru.push_front(Node<T,Y>(key,value));
+    cache.insert(make_pair(key,lru.begin()));
+    return;
+  }
+  lru.push_front(Node<T,Y>(key,value));
+  lru.erase(cache[key]);
+  cache[key] = lru.begin();
+}
 
+template<class T,class Y>
+optional<Y> LRUcache<T,Y>::getValueFromKey(T key){
+  if(cache.count(key)==0)return{}; 
+  lru.push_front(Node<T,Y>(key,cache[key]->value));
+  lru.erase(cache[key]);
+  cache[key] = lru.begin();
+  return cache[key]->value;
+}
+
+
+
+
+int main(){
+  LRUcache<string,int> c1(3);
+  c1.insertKeyValuePair("b",2);
+  c1.insertKeyValuePair("a",1);
+  c1.insertKeyValuePair("c",3);
+
+  c1.print_cache();
+  cout<<endl;
+  c1.print_lru();
+
+  cout<<endl<<c1.getMostRecentKey()<<endl;
+  
+  cout<<(*c1.getValueFromKey("c"))<<endl;
+  cout<<*c1.getValueFromKey("b")<<endl;
+  cout<<*c1.getValueFromKey("a")<<endl;
+
+  c1.print_cache();
+  cout<<endl;
+  c1.print_lru();
+
+  c1.insertKeyValuePair("d",4);
+  c1.insertKeyValuePair("e",5);
+
+  c1.print_cache();
+  cout<<endl;
+  c1.print_lru();
+
+
+  c1.insertKeyValuePair("d",2);
+  cout<<c1.getMostRecentKey()<<endl;
+
+  c1.print_cache();
+  cout<<endl;
+  c1.print_lru();
 }
